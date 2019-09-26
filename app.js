114a2ad11e4ab54a02ab1bf4153fc8eec7e0663c
Lester Dominguez <Lester.dominguez@gmail.com>
2019-09-26 11:25:14 -0400

    Added css styling and views files

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var morgan = require('morgan');
 const app = express();
+const layout = require("./views/layout")
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 app.use(morgan("dev"));
 app.use(express.static(__dirname + '/public'));
 
+
 app.get('/', function (req, res) {
-  res.send("hello world");
+  res.send(layout("hello world"));
 });
 
-const PORT = 1337;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
