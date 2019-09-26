6f1de48381ee89f22dc8ded1f52e5c5c69600710
Yooni Park <yoonikj@gmail.com>
2019-09-26 11:47:03 -0400

    added sequelize

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var morgan = require('morgan');
 const app = express();
-const layout = require("./views/layout")
+const layout = require("./views/layout");
+const models = require('./models');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 app.use(morgan("dev"));
 app.use(express.static(__dirname + '/public'));
-
 
 app.get('/', function (req, res) {
   res.send(layout("hello world"));
@@ -15,6 +15,13 @@ app.get('/', function (req, res) {
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-})
+const init = async () => {
+  await models.User.sync();
+  await models.Page.sync();
+
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  });
+}
+
+init();
